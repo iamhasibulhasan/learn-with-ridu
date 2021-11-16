e2c36f37f0f76f606ff36f9db3999cd2f32e82ab
imhasibulhasan <hashibul218@gmail.com>
2021-11-16 15:29:20 +0600

    style about us

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,8 +3,21 @@ import './About.css';
 
 const AboutUs = () => {
     return (
-        <div>
-            <h3>This is about us</h3>
+        <div className="container">
+            <div className="about d-lg-flex gx-4">
+                <div>
+                    <h5>About Our Education</h5>
+                    <h3>WELCOME TO NEW ERA ONLINE EDUCATION</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis alias veniam repellendus fuga iusto, similique soluta provident odit, possimus nisi corrupti quibusdam maiores amet odio facere ipsam nulla animi reiciendis eos. At dolor, mollitia ea dolorem fugiat porro cum eligendi, aperiam reiciendis corrupti fugit veniam nihil eveniet commodi. Aperiam a dolores ipsum soluta. Quas aliquam nisi vitae neque dolore eligendi beatae iusto, quidem earum fugit molestias blanditiis optio ducimus aperiam id quam amet voluptatum sapiente. Iusto, aperiam error quibusdam dolor dicta accusantium pariatur in, iste hic optio, eveniet nisi corporis quam. Eaque quas possimus deleniti odio illum laboriosam porro sed quos quia, fuga eum autem illo quisquam sequi voluptas impedit! Sapiente doloremque nostrum repudiandae tempora, aperiam nesciunt harum blanditiis mollitia itaque aspernatur assumenda exercitationem nihil est corrupti. Sequi aliquid soluta architecto numquam officia nisi at nihil perferendis quis, nesciunt aut animi optio expedita facere nostrum reiciendis quos. Minus, cumque pariatur?
+                    </p>
+                </div>
+                <div>
+                    <figure>
+                        <img src="	http://kodeforest.net/html/uoe/extra-images/abt-thumb.jpg" alt="" />
+                    </figure>
+                </div>
+            </div>
         </div>
     );
 };
