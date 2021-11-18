337491265516dfbefacf34d337b4027b4fccdaf8
imhasibulhasan <hashibul218@gmail.com>
2021-11-18 16:46:38 +0600

    edit our course section

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -8,25 +8,25 @@ const Category = () => {
             <h3 className="sec-title mb-4">Course Category</h3>
             <div className="category">
                 <div className="mid-text">
-                    <img src="http://kodeforest.net/html/uoe/extra-images/intro-1.jpg" />
+                    <img src="http://kodeforest.net/wp-demo/education/wp-content/uploads/2017/02/gallery1-350x350.jpg" />
                     <span>
                         <Link className="btn btn-warning" to="/courses">Category Name</Link>
                     </span>
                 </div>
                 <div className="mid-text">
-                    <img src="http://kodeforest.net/html/uoe/extra-images/intro-1.jpg" />
+                    <img src="http://kodeforest.net/wp-demo/education/wp-content/uploads/2017/02/gallery3-350x350.jpg" />
                     <span>
                         <Link className="btn btn-warning" to="/courses">Category Name</Link>
                     </span>
                 </div>
                 <div className="mid-text">
-                    <img src="http://kodeforest.net/html/uoe/extra-images/intro-1.jpg" />
+                    <img src="http://kodeforest.net/wp-demo/education/wp-content/uploads/2017/02/gallery6-350x350.jpg" />
                     <span>
                         <Link className="btn btn-warning" to="/courses">Category Name</Link>
                     </span>
                 </div>
                 <div className="mid-text">
-                    <img src="http://kodeforest.net/html/uoe/extra-images/intro-1.jpg" />
+                    <img src="http://kodeforest.net/wp-demo/education/wp-content/uploads/2017/02/gallery7-350x350.jpg" />
                     <span>
                         <Link className="btn btn-warning" to="/courses">Category Name</Link>
                     </span>
